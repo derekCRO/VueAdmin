fb8c4e8d48878c0fdaeaaad3673be0fb7597638c
intel9911 <tiop08317@gmail.com>
2019-02-14 13:25:11 +0000

    adding CRUD Table

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -52,6 +52,7 @@
                 <v-card-text>
                   Operations Contents
                   
+                  
                 </v-card-text>
               </v-card>
             </v-tab-item>
@@ -77,9 +78,80 @@
                 </v-card-text>
               </v-card>
             </v-tab-item>
-          </v-tabs>          
+          </v-tabs>
+          <v-subheader> DataTable Components</v-subheader>
+          <v-toolbar flat color="white">
+            <v-toolbar-title>Scheduling Control Codes</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+            <v-dialog v-model="dialog" max-width="700px">
+              <v-btn slot="activator" color="primary" dark class="mb-2">New Item</v-btn>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+                
+                <v-card-text>
+                  <v-container grid-list-md>
+                    <v-layout wrap>
+                      <v-flex xs12 sm6 md4>
+                        <v-text-field v-model="editedItem.code" label="Code"></v-text-field>
+                      </v-flex>
+                      <v-flex xs12 sm6 md4>
+                        <v-text-field v-model="editedItem.category" label="Category"></v-text-field>
+                      </v-flex>
+                      <v-flex xs12 sm6 md4>
+                        <v-text-field v-model="editedItem.description" label="Description"></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+          <v-data-table
+            :headers="headers"
+            :items="desserts"
+            class="elevation-1"
+            :search="search"
+          >
+            <template slot="items" slot-scope="props">
+              <td class="text-xs-center">{{ props.item.code }}</td>
+              <td class="text-xs-center">{{ props.item.category }}</td>
+              <td class="text-xs-center">{{ props.item.description }}</td>
+              <td class="justify-center layout px-0">
+                <v-icon
+                  small
+                  class="mr-2"
+                  @click="editItem(props.item)"
+                >
+                  edit
+                </v-icon>
+                <v-icon
+                  small
+                  @click="deleteItem(props.item)"
+                >
+                  delete
+                </v-icon>
+              </td>
+            </template>
+            <template slot="no-data">
+              <v-btn color="primary" @click="initialize">Reset</v-btn>
+            </template>
+          </v-data-table>          
         </v-flex>
-     
       </v-layout>
     </v-container>
   </div>
@@ -126,6 +198,31 @@ export default {
     codes: Codes,
     color: Material,
     selectedTab: 'tab-1',
+    dialog: false,
+    headers: [
+      {
+        text: 'Code',
+        align: 'center',
+        sortable: false,
+        value: 'code'
+      },
+      { text: 'Category',align: 'center', sortable: false,  value: 'category' },
+      { text: 'Description', align: 'center', sortable: false,  value: 'description' },
+      { text: 'Actions', value: 'name', align: 'center', sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      code: '',
+      category: '',
+      description: ''
+    },
+    defaultItem: {
+      code: '',
+      category: '',
+      description: ''
+    },
+    search: '',
     linearTrending: [
       {
         subheading: 'Sales',
@@ -216,6 +313,9 @@ export default {
     ]    
   }),
   computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Code'
+    },
     activity () {
       return API.getActivity();
     },
@@ -229,6 +329,98 @@ export default {
       return API.getLocation;
     }
   },
+  watch: {
+    dialog (val) {
+      val || this.close()
+    }
+  },
+  created () {
+    this.initialize()
+  },
+  methods: {
+    initialize () {
+      this.desserts = [
+        {
+          code: '1-1 MEETING WITH',
+          category: 'Other',
+          description: '1-1 Meeting with edmen'
+        },
+        {
+          code: '1ST',
+          category: 'Training',
+          description: '1st Aid Training'
+        },
+        {
+          code: 'ACTING UP',
+          category: 'Scheduled Shift',
+          description: 'ACTING UP'
+        },
+        {
+          code: 'AS',
+          category: 'Other',
+          description: 'Adjusted Shift'
+        },
+        {
+          code: 'AD',
+          category: 'Scheduled Shift',
+          description: 'Admin Day'
+        },
+        {
+          code: 'APO',
+          category: 'Other',
+          description: 'Advanced Pull Out'
+        },
+        {
+          code: 'L',
+          category: 'Annual Leave',
+          description: 'Annual Leave'
+        },
+        {
+          code: 'BF',
+          category: 'Other',
+          description: 'Back Fill'
+        },
+        {
+          code: 'B',
+          category: 'Other',
+          description: 'Berevement Leave'
+        },
+        {
+          code: 'BC',
+          category: 'Training',
+          description: 'Bowel care'
+        }
+       ]
+    },
+
+    editItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      const index = this.desserts.indexOf(item)
+      confirm('Are you sure you want to delete this code?') && this.desserts.splice(index, 1)
+    },
+
+    close () {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      }, 300)
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
+      }
+      this.close()
+    }
+  }
 
 };
 </script>
