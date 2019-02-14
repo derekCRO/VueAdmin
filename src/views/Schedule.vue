6006aaffd38734c989e6092c6314eb5a375d48b8
intel9911 <tiop08317@gmail.com>
2019-02-14 21:44:57 +0000

    Tabs Update

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -676,7 +676,7 @@ export default {
   data: () => ({
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',      
     e1: null,
-    codes: Codes,
+    codes: [],
     color: Material,
     selectedTab: 'tab-1',
     dialog: false,
@@ -873,7 +873,8 @@ export default {
           category: 'Training',
           description: 'Bowel care'
         }
-       ]
+       ],
+       this.codes=this.desserts
     },
 
     editItem (item) {
