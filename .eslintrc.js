b74454a5419520d25168056642524faf741b5c91
intel9911 <tiop08317@gmail.com>
2019-02-16 17:31:00 +0000

    Menu Issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
+    root: true,
+    env: {
       node: true
-  },
-  extends: [
-      'plugin:vue/essential',
-      'eslint:recommended',
-      '@vue/prettier'
-  ],
-  rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      // 'vue/script-indent': ['error', 4, {'baseIndent': 1}],
-      indent: ['error', 4]
-  },
-  parserOptions: {
-      parser: 'babel-eslint'
-  }
+    },
+    extends: ["plugin:vue/essential", "@vue/prettier"],
+    rules: {
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    },
+    parserOptions: {
+      parser: "babel-eslint"
+    }
 };
+  
